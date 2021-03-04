ea74d71c650fb89d1fdbe8a49e7da51f88cae3c6
vincentwxie <vincentwxie@tencent.com>
2021-03-04 12:04:14 +0800

    feat: add medium

---
+++ easy/palindrome-number/index.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=9 lang=javascript
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if (x < 0) {
+    return false;
+  }
+  const str = x.toString();
+  let isPalindrome = true;
+  const l = str.length;
+  const halfL = Math.floor(l / 2);
+  for (let i = 0; i < halfL; i++) {
+    if (str[i] !== str[l - i - 1]) {
+      isPalindrome = false;
+      break;
+    }
+  }
+  return isPalindrome;
+};
+// @lc code=end
+
---
+++ easy/roman-to-integer/index.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=13 lang=javascript
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const valueMap = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    };
+    if (s.length === 1) {
+      return valueMap[s];
+    }
+    let i = 0,
+      j = 1,
+      total = 0;
+    while (j < s.length) {
+      const forward = valueMap[s[i]];
+      const afterward = valueMap[s[j]];
+      if (forward >= afterward) {
+        total += forward;
+        i++;
+        j++;
+      } else {
+        total += afterward - forward;
+        i += 2;
+        j += 2;
+      }
+    }
+    if (j === s.length) {
+      total += valueMap[s[j - 1]];
+    }
+    return total;
+};
+// @lc code=end
+
